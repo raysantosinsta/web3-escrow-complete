e544071bb551e0c9f344bd23e8edb9b7be60910a
ray santos <raysantosinsta@gmail.com>
2026-04-14 01:10:20 -0300

    aplicado seguranca

--- app/constants/contracts.ts
+++ app/constants/contracts.ts
@@ -1,7 +1,6 @@
 // frontend/app/constants/contracts.ts
 export const ESCROW_ADDRESS = (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0x5FbDB2315678afecb367f032d93F642f64180aa3") as `0x${string}`;
 
-
 export const ESCROW_ABI = [
   {
     "inputs": [
@@ -15,12 +14,42 @@ export const ESCROW_ABI = [
     "type": "function"
   },
   {
-    "inputs": [
-      { "internalType": "uint256", "name": "id", "type": "uint256" }
-    ],
+    "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }],
     "name": "release",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }],
+    "name": "markAsDelivered",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }],
+    "name": "openDispute",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }],
+    "name": "refund",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "id", "type": "uint256" },
+      { "internalType": "uint256", "name": "releaseToSellerAmount", "type": "uint256" },
+      { "internalType": "uint256", "name": "refundToBuyerAmount", "type": "uint256" }
+    ],
+    "name": "resolveDispute",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
-];
+] as const;
